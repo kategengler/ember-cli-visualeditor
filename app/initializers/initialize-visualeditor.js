e629ec8fb479a4f8cbdab771a3e59bc77e67e189
Oliver Buchtala <oliver.buchtala@gmail.com>
2014-12-29 19:22:34 +0200

    Deactivate global initializer.
    We want to be able to load ve lazily.

--- app/initializers/initialize-visualeditor.js
+++ app/initializers/initialize-visualeditor.js
@@ -1,19 +1,19 @@
 /* globals ve:true, OO:true, $:true */
 
 function initialize( container, application ) {
-  application.deferReadiness();
+  // application.deferReadiness();
 
-  // At the moment we only package the english version
-  // TODO: add all languages to /index.js (app.import)
-  $.i18n().locale = "en";
+  // // At the moment we only package the english version
+  // // TODO: add all languages to /index.js (app.import)
+  // $.i18n().locale = "en";
 
-  ve.init.platform = new ve.init.sa.Platform();
-  OO.ui.getUserLanguages = ve.init.platform.getUserLanguages.bind( ve.init.platform );
-  OO.ui.msg = ve.init.platform.getMessage.bind( ve.init.platform );
-  ve.init.platform.addMessagePath('./visualeditor/i18n/');
-  ve.init.platform.initialize().done(function () {
-    application.advanceReadiness();
-  });
+  // ve.init.platform = new ve.init.sa.Platform();
+  // OO.ui.getUserLanguages = ve.init.platform.getUserLanguages.bind( ve.init.platform );
+  // OO.ui.msg = ve.init.platform.getMessage.bind( ve.init.platform );
+  // ve.init.platform.addMessagePath('./visualeditor/i18n/');
+  // ve.init.platform.initialize().done(function () {
+  //   application.advanceReadiness();
+  // });
 
 }
 
