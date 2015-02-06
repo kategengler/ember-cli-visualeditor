5e3b841280a47835b1e09accf293a9983773c134
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-02-06 13:53:40 +0100

    Use jquery to load scripts.
    Script injection did not work in phantomjs, for instance.
    
    Plus: added switch to load minified version (environment=production).

--- addon/initializers/initialize_visual_editor.js
+++ addon/initializers/initialize_visual_editor.js
@@ -2,88 +2,79 @@
 
 var _loadedScripts = {};
 
-var _scripts = [
-  '/visual-editor/jquery-i18n.min.js',
-  '/visual-editor/jquery-uls.min.js',
-  '/visual-editor/oojs.min.js',
-  '/visual-editor/oojs-ui.min.js',
-  '/visual-editor/visualEditor.min.js'
-];
-
-var styleSheets = [
-  '/visual-editor/oojs-ui-apex.svg.css',
-  '/visual-editor/visualEditor.css'
-];
-
-var injectScript = function(el, src) {
-  return function() {
-    var promise = window.jQuery.Deferred();
-    var scriptEl = window.document.createElement('script');
-    scriptEl.type = "text\/javascript";
-    scriptEl.src = src;
-    scriptEl.onload = function() {
-      promise.resolve();
-      _loadedScripts[src] = true;
-    };
-    scriptEl.onerror = function (error) {
-      console.error('Could not load', src);
-      promise.reject(new URIError("The script " + error.target.src + " is not accessible."));
-    };
-    el.appendChild(scriptEl);
-    return promise;
-  };
-};
-
-var loadScripts = function(scripts) {
-  var promise = window.jQuery.Deferred();
-  promise.resolve();
+// This version injects a script instead of using global.eval
+// which eases debugging (e.g., stacktraces make sense)
+var loadScriptDebug = function(src) {
   var headEl = document.head || document.getElementsByTagName("head")[0];
-  for (var i = 0; i < scripts.length; i++) {
-    var src = scripts[i];
-    if (_loadedScripts[src]) {
-      continue;
-    }
-    promise = promise.then(injectScript(headEl, src));
-  }
+  var promise = window.jQuery.Deferred();
+  var scriptEl = window.document.createElement('script');
+  scriptEl.type = "text\/javascript";
+  scriptEl.src = src;
+  scriptEl.onload = function() {
+    promise.resolve();
+    _loadedScripts[src] = true;
+  };
+  scriptEl.onerror = function (error) {
+    console.error('Could not load', src);
+    promise.reject(new URIError("The script " + error.target.src + " is not accessible."));
+  };
+  headEl.appendChild(scriptEl);
   return promise;
 };
 
-var initializeVisualEditor = function(routePrefix) {
-  if (routePrefix === undefined) {
-    routePrefix = "";
-  }
-
-  var scripts = [];
-  for (var i = 0; i < _scripts.length; i++) {
-    scripts.push(routePrefix + _scripts[i]);
-  }
-
-  var promise = loadScripts(scripts)
-    .done(function() {
-      styleSheets.forEach(function(href) {
-        if (!_loadedScripts[href]) {
-          $('<link/>', {
-             rel: 'stylesheet',
-             type: 'text/css',
-             href: routePrefix + href
-          }).appendTo('head');
-          _loadedScripts[href] = true;
-        }
-      });
-      // HACK: this produces a failing request with fallback to 'en'
-      // so we use 'en' right away
-      if ($.i18n().locale === "en-US") {
-        $.i18n().locale = "en";
-      }
-
-      ve.init.platform.addMessagePath(routePrefix + '/visual-editor/i18n/oojs-ui/');
-      ve.init.platform.addMessagePath(routePrefix + '/visual-editor/i18n/ve/');
-
-      return ve.init.platform.initialize();
-    })
-    .fail(function() {
-      console.error('Failed to load assets for ember-cli-visualeditor', arguments);
+var loadScript = function(src) {
+  if (_loadedScripts[src]) {
+    return window.jQuery.Deferred().resolve();
+  } else {
+    return $.getScript(src).done(function() {
+      _loadedScripts[src] = true;
     });
+  }
+};
+
+var initializeVisualEditor = function(options) {
+  options = options || {};
+  var routePrefix = options.routePrefix || "";
+
+  var promise;
+  var scriptSrc;
+
+  if (options.environment === "production") {
+    scriptSrc = routePrefix + "/visual-editor/visual-editor.min.js";
+  } else {
+    scriptSrc = routePrefix + "/visual-editor/visual-editor.js";
+  }
+
+  if (options.debug) {
+    promise = loadScriptDebug(scriptSrc);
+  } else {
+    promise = loadScript(scriptSrc);
+  }
+
+  promise.done(function() {
+    var stylesheet = routePrefix + "/visual-editor/visual-editor.css";
+    if (!_loadedScripts[stylesheet]) {
+      $('<link/>', {
+         rel: 'stylesheet',
+         type: 'text/css',
+         href: stylesheet
+      }).appendTo('head');
+      _loadedScripts[stylesheet] = true;
+    }
+    // HACK: this produces a failing request with fallback to 'en'
+    // so we use 'en' right away
+    if ($.i18n().locale === "en-US") {
+      $.i18n().locale = "en";
+    }
+
+    ve.init.platform.addMessagePath(routePrefix + '/visual-editor/i18n/oojs-ui/');
+    ve.init.platform.addMessagePath(routePrefix + '/visual-editor/i18n/ve/');
+
+    return ve.init.platform.initialize();
+  }).fail(function() {
+    console.error('Failed to load assets for ember-cli-visualeditor', arguments);
+  });
+
   return promise;
 };
 
