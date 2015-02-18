bf6640b578a1d90a80f558913cedc9b22c1af83f
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-02-19 00:45:20 +0100

    Use npm merge.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* jshint node: true */
 'use strict';
 
-var _ = require('lodash-node');
+var merge = require('merge');
 
 module.exports = {
   name: 'ember-cli-visualeditor',
@@ -18,7 +18,7 @@ module.exports = {
     // 1. app/config/environment.js
     // 2. app/Brocfile.js (EmberApp options)
     // 3. addon/configuration/environment.js
-    return _.merge(this.config(app.env)[this.name],
+    return merge(this.config(app.env)[this.name],
         this.options,
         this.project.config(app.env)[this.name]);
   },
